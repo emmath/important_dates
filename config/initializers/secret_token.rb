c66e0b923aab54b452b6b8917e25853dcbb0d17a
emmath <emmatrevena@gmail.com>
2013-07-16 23:08:00 -0700

    important dates application generated, css added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImportantDates::Application.config.secret_token = '9af439bdc5c17ed4207fcb42c886d6059e3cc24ccbc5b00653b17f3851e79ab7f41fbbd3bfdc16c0fee4879d8c382378150b4e038a23707bcc3cb467ecf9003e'
